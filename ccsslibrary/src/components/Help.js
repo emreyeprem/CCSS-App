66b8e57bb6d1903bbb55b134eb56a6e2cd3772f6
Emre Akurek <emreyeprem@gmail.com>
2019-01-16 20:10:04 -0600

    help page is completed

--- ccsslibrary/src/components/Help.js
+++ ccsslibrary/src/components/Help.js
@@ -67,13 +67,16 @@ class Help extends Component{
       <div id="carouselExampleControls" className="carousel slide slideHeight" data-ride="carousel">
         <div className="carousel-inner">
             <div className="carousel-item active ">
-                <img className="d-block w-100 slideHeight" src="https://www.dailyherald.com/storyimage/da/20190114/news/190119820/EP/1/3/EP-190119820.jpg&updated=201901140932&imageversion=Facebook&exactH=630&exactW=1200&exactfit=crop&noborder" alt="First slide"/>
+                <img className="d-block w-100 slideHeight" src="https://i2.wp.com/blog.signalnoise.com/wp-content/uploads/2010/08/i_backtoschool.jpg" alt="First slide"/>
+                <img className="d-block w-100 slideHeight" src="https://ecdn.teacherspayteachers.com/thumbitem/LAB-SAFETY-POSTERS-Secondary-Science-humor--2692109-1471818151/original-2692109-1.jpg" alt="Second slide"/>
             </div>
             <div className="carousel-item ">
                 <img className="d-block w-100 slideHeight" src="https://ecdn.teacherspayteachers.com/thumbitem/Exponent-Rules-Coloring-Sheet-2201651-1460014812/original-2201651-1.jpg" alt="Second slide"/>
+                <img className="d-block w-100 slideHeight" src="https://ecdn.teacherspayteachers.com/thumbitem/-1-Deals-Nouns-Worksheets-4200328-1542330001/original-4200328-1.jpg" alt="First slide"/>
             </div>
             <div className="carousel-item ">
                 <img className="d-block w-100 slideHeight" src="https://ecdn.teacherspayteachers.com/thumbitem/Division-using-Repeated-Subtraction-098785800-1379881223-1516786197/original-885131-4.jpg" alt="Third slide"/>
+                <img className="d-block w-100 slideHeight" src="http://cialiswow.com/wp-content/uploads/2018/08/free-printable-abacus-worksheets-pattern-cards-homeschool-deals-575x300.jpg" alt="Third slide"/>
             </div>
         </div>
         <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -99,15 +102,27 @@ class Help extends Component{
 
      <div className="containerBoxQ">
      <br />
-     <div className="row site_content numberHeading">
+     <div className="clearfix numberHeading">
 
-         <div className="col-md-1 faq_num ">1</div>
-         <div Name="col-md-10 faq ">
-             <h3 className="faq_question">How do I apply?</h3>
+      <div className="col-md-1 faq_num nmbrBox">1</div>
+     <div className="col-md-10 faq">
+         <h3 className="faq_question">{"What if my file isn't printing correctly?"}</h3>
+          <div className="faq_answer">
+          <p>{"First, determine what file type you're having trouble printing. If it's a ZIP file, you'll need to uncompress the file first before you can print the components. Here's how you can open a Zip file. If the file is a PDF, especially a PDF with a lot of graphics, it may look fine on your screen, but may not print out correctly. If you’re having trouble printing a PDF, please be sure that you're opening the PDF with the latest version of Adobe Acrobat Reader."}
+          </p>
+          </div>
+     </div>
+     </div>
+
+         <div className="clearfix numberHeading">
+
+        	<div className="col-md-1 faq_num nmbrBox">2</div>
+         <div className="col-md-10 faq">
+             <h3 className="faq_question">How and when do I get paid?</h3>
               <div className="faq_answer">
-              <p>
-                  You can apply online to PTCL after searching for openings/vacancies relevant to your area of interest.
-                  However, if there is no opening/vacancy, currently, to your area of interest, you can still leave your information with us by clicking on My Profile.
+              <p>We work with third party service providers to issue your payments to you. The provider we currently work with is Paypal. In order to get paid, you'll need to have an account with PayPal and be sure that your account is eligible and ready to receive payments. Finally, make sure that you've provided this information in the "Payment Options" section of your TpT account information.
+
+
               </p>
               </div>
          </div>
@@ -115,23 +130,11 @@ class Help extends Component{
 
          <div className="clearfix numberHeading">
 
-        	<div className="col-md-1 faq_num">2</div>
+         <div className="col-md-1 faq_num nmbrBox">3</div>
          <div className="col-md-10 faq">
-             <h3 className="faq_question">I cannot find a suitable job on your website. Can I still apply?</h3>
+             <h3 className="faq_question">What are CCSS credits?</h3>
               <div className="faq_answer">
-              <p>Yes. You can update your profile in the My Profile section and specify you area of interest(s) according to your desired role. Once you save your profile, your information will be saved into our Talent Pool and a member of our Talent Acquisition team will contact you with relevant opportunities as they arise.
-              </p>
-              </div>
-         </div>
-         </div>
-
-         <div className="clearfix numberHeading">
-
-         <div className="col-md-1 faq_num">3</div>
-         <div className="col-md-10 faq">
-             <h3 className="faq_question">If I submit an application or submit my details, will PTCL keep me on file?</h3>
-              <div className="faq_answer">
-              <p>Yes. After you apply on our website at www.ptcl.com.pk/careers or create your profile, your information will be saved into our Talent Pool.
+              <p>{"CCSS credits are points which can be applied to future purchases to save you money. To earn credits: After you make a purchase, you'll need to leave feedback on your purchase. We recommend waiting to do so until you've had a chance to use the product. When you're ready to leave feedback, just return to My Purchases and click the Provide Feedback link to the right of the date purchased. For every dollar you spend on TpT, you'll earn 1 credit—and we'll round up for you, too! If you provide fair feedback on a $4.75 item, you will earn 5 credits. Every 20 credits you earn equals $1 to apply toward future TpT purchases. Please note that TpT credits are only earned for feedback left on active, paid teacher-created resources. There isn’t an option to leave feedback when you purchase TpT gift cards so credits aren’t earned for them. Credits are not earned for feedback left on free downloads either."}
               </p>
               </div>
          </div>
@@ -153,26 +156,26 @@ class Help extends Component{
             <div className="col-md-4 col-12 px-2">
                 <div className="text-center"><i><span className="fa fa-user fa-5x"></span></i></div>
                 <p className="mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto
-                consequatur cum eum laborum magni nam necessitatibus nisi? Ad cumque debitis
-                dolore eum hic laboriosam nesciunt odit officia omnis similique.
+                <h6>How do I cancel my account?</h6>
+{"We're sorry to see you go! Keep in mind that if you close your account, you won’t be able to download items any longer, even the ones you’ve already paid for. If you’d just like to turn off your notifications, you can do so by clicking Email Preferences on the “My Account” section of your profile, and unsubscribing from any emails you would prefer not to receive. If you’d still like to cancel your account, please follow the steps below: Hover over My CCSS and click on My Account Click the red Edit button at the bottom of the page then scroll down to the bottom and click “Close My Account Now” and press OK."}
 
                 </p>
                     </div>
             <div className="col-md-4 col-12 px-2">
                 <div className="text-center"><i><span className="fa fa-industry fa-5x"></span></i></div>
                 <p className="mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto
-                consequatur cum eum laborum magni nam necessitatibus nisi? Ad cumque debitis
-                dolore eum hic laboriosam nesciunt odit officia omnis similique.
+                <h6>Can I post the same resource with a different title?</h6>
+No. To help avoid Buyer confusion, please don’t post the same resource more than once. If there are substantive differences that truly change the nature of the resource, feel free to post as a separate resource and clearly outline the differences in your resource description. If it’s brought to our attention that a Seller has been posting duplicate resources, we’ll reach out and ask that Seller to remove them. This prohibition does not apply to posting resources combined into bundles, although the Seller should be as clear as possible about what the bundle contains to avoid confusion.
                </p>
             </div>
             <div className="col-md-4 col-12 px-2">
                 <div className="text-center"><i><span className="fa fa-dashboard fa-5x"></span></i></div>
                  <p className="mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto
-                consequatur cum eum laborum magni nam necessitatibus nisi? Ad cumque debitis
-                dolore eum hic laboriosam nesciunt odit officia omnis similique.
+                 <h6>How do I search for resources?</h6>
+ From the TpT home page, you have a few options:
+ 1. Use the search bar at the top of the page—just type in a keyword and go. You can sort your results by relevance, price, rating, recency or best selling.<br/>
+ 2. Use the category labels on the left side of the page to narrow your search, then browse. You can select one or more options:
+ 3. Once you've found some products you like, follow those Sellers by clicking the "Follow Me" link right under their store name. You'll then receive a notification each time they post something new.
                 </p>
             </div>
         </div>
@@ -180,10 +183,10 @@ class Help extends Component{
       <div className="row">
           <div class="col-12 col-md-5 p-5">
               <address className="m-lg-5">
-                  <b className="d-block">Twitter, Inc.</b>
-                  <p>1355 Market St, Suite 900</p>
-                  <p>San Francisco, CA 94103</p>
-                  <p> <abbr title="Phone Number">P</abbr>: (123) 456-7890 </p>
+                  <b className="d-block">Common Core Library, Inc.</b>
+                  <p>1255 Down Hill St, Suite 700</p>
+                  <p>Houston, TX 77103</p>
+                  <p> <abbr title="Phone Number">Tel</abbr>: &nbsp;(732) 456-6890 </p>
               </address>
           </div>
           <div className="col-12 col-md-7">
